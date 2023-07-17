dcc5b4becbe52cbd18977538fd1b12ad95e7b779
xiepengcz <949026370@qq.com>
2023-07-17 23:43:38 +0800

    edit-doc

--- src/reactivity/test/effect.spec.ts
+++ src/reactivity/test/effect.spec.ts
@@ -34,7 +34,8 @@ describe("effect", () => {
   });
 
   it("scheduler", () => {
-    // 1.通过 effect 第二个参数，给定一个 scheduler 的 fn
+    // scheduler:调度程序
+    // 1.通过 effect 第二个参数，给定一个 scheduler 的 fn 
     // 2.effect 首次执行时，会执行 fn
     // 3.当响应式对象 set update 时，不会执行fn，而是执行 scheduler
     // 4.如果当执行 runner 时，会再次执行 fn
