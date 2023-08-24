c1fcce36da5155702f108e6e7a283fe790d469be
xiepengcz <949026370@qq.com>
2023-08-24 19:24:08 +0800

    实现getCurrentInstance

--- src/runtime-core/component.ts
+++ src/runtime-core/component.ts
@@ -30,12 +30,13 @@ function setupStatefulComponent(instance: any) {
 
   const { setup } = Component;
   if (setup) {
+    setCurrentInstance(instance);
     // 因为用户不一定会写 setup 所以要判断一下
     // setup 可能是 Object 也可能是 function
     const setupResult = setup(shallowReadonly(instance.props), {
       emit: instance.emit,
     }); // props表层是不可变的,但深层次是可变的，所以用shallowReadonly处理,
-
+    setCurrentInstance(null);
     handleSetupResult(setupResult, instance);
   }
 }
@@ -53,4 +54,14 @@ function finishComponentSetup(instance: any) {
   const Component = instance.type;
 
   instance.render = Component.render;
+}
+
+let currentInstance = null;
+
+export function getCurrentInstance() {
+  return currentInstance;
+}
+
+function setCurrentInstance(instance) {
+  currentInstance = instance;
 }
