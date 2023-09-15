aadfedf1539dd1544c8c12db04d615eb243ce03d
xiepengcz <949026370@qq.com>
2023-09-15 18:28:38 +0800

    实现中间对比和移动的diff算法

--- example/patchChildren/ArrayToArray.js
+++ example/patchChildren/ArrayToArray.js
@@ -52,14 +52,14 @@ import { h, ref } from "../../lib/guide-mini-vue.esm.js";
 // (a b)
 // d c (a b)
 // i= 2, e1 = -1; e2 = 0;
-const prevChildren = [h("div", { key: "A" }, "A"), h("div", { key: "B" }, "B")];
+// const prevChildren = [h("div", { key: "A" }, "A"), h("div", { key: "B" }, "B")];
 
-const nextChildren = [
-  h("div", { key: "D" }, "D"),
-  h("div", { key: "C" }, "C"),
-  h("div", { key: "A" }, "A"),
-  h("div", { key: "B" }, "B"),
-];
+// const nextChildren = [
+//   h("div", { key: "D" }, "D"),
+//   h("div", { key: "C" }, "C"),
+//   h("div", { key: "A" }, "A"),
+//   h("div", { key: "B" }, "B"),
+// ];
 
 // 4、老的比新的长 删除老的
 // 左侧
@@ -88,6 +88,138 @@ const nextChildren = [
 // ];
 
 // const nextChildren = [h("div", { key: "B" }, "B"), h("div", { key: "C" }, "C")];
+// 5、 对比中间的部分
+// 删除老的（在老的里面存在，新的里面不存在）
+// 5.1
+// a b (c d) f g
+// a b (e c) f g
+// d 节点在新的里面没有，需要删除掉
+// c 节点在 props 也发生了变化
+// const prevChildren = [
+//   h("div", { key: "A" }, "A"),
+//   h("div", { key: "B" }, "B"),
+//   h("div", { key: "C", id: "c-prev" }, "C"),
+//   h("div", { key: "D" }, "D"),
+//   h("div", { key: "F" }, "F"),
+//   h("div", { key: "G" }, "G"),
+// ];
+
+// const nextChildren = [
+//   h("div", { key: "A" }, "A"),
+//   h("div", { key: "B" }, "B"),
+//   h("div", { key: "E" }, "E"),
+//   h("div", { key: "C", id: "c-next" }, "C"),
+//   h("div", { key: "F" }, "F"),
+//   h("div", { key: "G" }, "G"),
+// ];
+
+// 5.1.1
+// a b (c e d) f g
+// a b (e c) f g
+// 中间部分，老的比新的多，多出来的直接删除（优化删除逻辑）
+// const prevChildren = [
+//   h("div", { key: "A" }, "A"),
+//   h("div", { key: "B" }, "B"),
+//   h("div", { key: "C", id: "c-prev" }, "C"),
+//   h("div", { key: "E" }, "E"),
+//   h("div", { key: "D" }, "D"),
+//   h("div", { key: "F" }, "F"),
+//   h("div", { key: "G" }, "G"),
+// ];
+
+// const nextChildren = [
+//   h("div", { key: "A" }, "A"),
+//   h("div", { key: "B" }, "B"),
+//   h("div", { key: "E" }, "E"),
+//   h("div", { key: "C", id: "c-next" }, "C"),
+//   h("div", { key: "F" }, "F"),
+//   h("div", { key: "G" }, "G"),
+// ];
+
+// 5.2 移动 节点存在于新的和老的里面，但是位置变了
+// 5.2.1
+// a b (c d e) f g
+// a b (e c d) f g
+// 最长子序列 ： [1,2]
+// const prevChildren = [
+//   h("div", { key: "A" }, "A"),
+//   h("div", { key: "B" }, "B"),
+//   h("div", { key: "C" }, "C"),
+//   h("div", { key: "D" }, "D"),
+//   h("div", { key: "E" }, "E"),
+//   h("div", { key: "F" }, "F"),
+//   h("div", { key: "G" }, "G"),
+// ];
+// const nextChildren = [
+//   h("div", { key: "A" }, "A"),
+//   h("div", { key: "B" }, "B"),
+//   h("div", { key: "E" }, "E"),
+//   h("div", { key: "C" }, "C"),
+//   h("div", { key: "D" }, "D"),
+//   h("div", { key: "F" }, "F"),
+//   h("div", { key: "G" }, "G"),
+// ];
+// 5.3 创建新的节点
+// a b (c e) f g
+// a b (e c d) f g
+// d 节点在老的节点中不存在，新的里面存在，则进行创建
+// const prevChildren = [
+//   h("div", { key: "A" }, "A"),
+//   h("div", { key: "B" }, "B"),
+//   h("div", { key: "C" }, "C"),
+//   h("div", { key: "E" }, "E"),
+//   h("div", { key: "F" }, "F"),
+//   h("div", { key: "G" }, "G"),
+// ];
+// const nextChildren = [
+//   h("div", { key: "A" }, "A"),
+//   h("div", { key: "B" }, "B"),
+//   h("div", { key: "E" }, "E"),
+//   h("div", { key: "C" }, "C"),
+//   h("div", { key: "D" }, "D"),
+//   h("div", { key: "F" }, "F"),
+//   h("div", { key: "G" }, "G"),
+// ];
+
+// 综合例子
+// a b (c d e z) f g
+// a b (d c y e) f g
+// const prevChildren = [
+//   h("div", { key: "A" }, "A"),
+//   h("div", { key: "B" }, "B"),
+//   h("div", { key: "C" }, "C"),
+//   h("div", { key: "D" }, "D"),
+//   h("div", { key: "E" }, "E"),
+//   h("div", { key: "Z" }, "Z"),
+//   h("div", { key: "F" }, "F"),
+//   h("div", { key: "G" }, "G"),
+// ];
+// const nextChildren = [
+//   h("div", { key: "A" }, "A"),
+//   h("div", { key: "B" }, "B"),
+//   h("div", { key: "D" }, "D"),
+//   h("div", { key: "C" }, "C"),
+//   h("div", { key: "Y" }, "Y"),
+//   h("div", { key: "E" }, "E"),
+//   h("div", { key: "F" }, "F"),
+//   h("div", { key: "G" }, "G"),
+// ];
+
+// fix  C 节点是move的，不是 create 的 
+const prevChildren = [
+  h("div", { key: "A" }, "A"),
+  h("div", { }, "C"),
+  h("div", { key: "B" }, "B"),
+  h("div", { key: "D" }, "D"),
+
+];
+const nextChildren = [
+  h("div", { key: "A" }, "A"),
+  h("div", { key: "B" }, "B"),
+  h("div", {  }, "C"),
+  h("div", { key: "D" }, "D"),
+
+];
 
 export default {
   setup() {
