dfc192f395c0423b4067cb3e9879996c983a2b21
xiepengcz <949026370@qq.com>
2023-09-20 12:15:52 +0800

    实现nextTick功能

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -4,6 +4,7 @@ import { ShapeFlags } from "../shared/ShapeFlags";
 import { createComponentInstance, setupComponent } from "./component";
 import { shouldUpdateComponent } from "./componentUpdateUtils";
 import { createAppAPI } from "./createApp";
+import { queueJobs } from "./scheduler";
 import { Fragment, Text } from "./vnode";
 
 export function createRenderer(options) {
@@ -367,34 +368,43 @@ export function createRenderer(options) {
   }
 
   function setupRenderEffect(instance, initialVNode, container, anchor) {
-    instance.update = effect(() => {
-      if (!instance.isMounted) {
-        console.log("init");
-        const { proxy } = instance;
+    instance.update = effect(
+      () => {
+        if (!instance.isMounted) {
+          console.log("init");
+          const { proxy } = instance;
 
-        const subTree = (instance.subTree = instance.render.call(proxy));
-        // vnode -> patch
-        // 将vnode 变成 element  然后 mountElement
-        patch(null, subTree, container, instance, anchor);
+          const subTree = (instance.subTree = instance.render.call(proxy));
+          // vnode -> patch
+          // 将vnode 变成 element  然后 mountElement
+          patch(null, subTree, container, instance, anchor);
 
-        // 所有的 element 都 mount 之后，再处理 $el
-        initialVNode.el = subTree.el;
-        instance.isMounted = true;
-      } else {
-        console.log("update");
-        const { next, vnode } = instance;
-        if (next) {
-          next.el = vnode.el;
-          updateComponentPreRender(instance, next);
+          // 所有的 element 都 mount 之后，再处理 $el
+          initialVNode.el = subTree.el;
+          instance.isMounted = true;
+        } else {
+          console.log("update");
+          const { next, vnode } = instance;
+          if (next) {
+            next.el = vnode.el;
+            updateComponentPreRender(instance, next);
+          }
+          const { proxy } = instance;
+
+          const subTree = instance.render.call(proxy);
+          const prevSubTree = instance.subTree;
+          instance.subTree = subTree;
+          patch(prevSubTree, subTree, container, instance, anchor);
         }
-        const { proxy } = instance;
-
-        const subTree = instance.render.call(proxy);
-        const prevSubTree = instance.subTree;
-        instance.subTree = subTree;
-        patch(prevSubTree, subTree, container, instance, anchor);
+      },
+      {
+        scheduler: () => {
+          console.log("update shceduler");
+          queueJobs(instance.update)
+        },
       }
-    });
+    );
+
   }
   return {
     createApp: createAppAPI(render),
